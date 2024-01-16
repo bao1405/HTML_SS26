508c3369a44ca9819718bf5829ae84dcac523ab3
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-16 16:57:49 +0700

    Update bai6.js

--- bai6.js
+++ bai6.js
@@ -2,10 +2,10 @@ const mangSoNguyen = [2, -5, 1, -3, 8, -7, 4, 6, -9];
 let indexAm = 0;
 let indexDuong = mangSoNguyen.length - 1;
 while (indexAm < indexDuong) {
-    while (mangSoNguyen[indexAm] >= 0 && indexAm < indexDuong) {
+    while (mangSoNguyen[indexAm] < 0 && indexAm < indexDuong) {
         indexAm++;
     }
-    while (mangSoNguyen[indexDuong] < 0 && indexAm < indexDuong) {
+    while (mangSoNguyen[indexDuong] >= 0 && indexAm < indexDuong) {
         indexDuong--;
     }
     if (indexAm < indexDuong) {
@@ -14,5 +14,5 @@ while (indexAm < indexDuong) {
         mangSoNguyen[indexDuong] = temp;
     }
 }
-console.log("Mảng sau khi di chuyển các phần tử âm lên đầu và dương về cuối:");
+console.log("Mảng sau khi di chuyển các số âm lên đầu và dương về cuối:");
 console.log(mangSoNguyen);
